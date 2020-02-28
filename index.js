c68c012687eb9db028929cda4903ca3ab81dacd6
Javier <javier.fuentesm@hotmail.com>
2020-02-27 19:09:05 -0600

    Custom Types

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 "use strict";
-const { graphql, buildSchema } = require("graphql");
-const express= require('express')
-const gqlMiddleware = require('express-graphql')
+const { buildSchema } = require("graphql");
+const express = require("express");
+const gqlMiddleware = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
+const app = express();
+const port = process.env.port || 3000;
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
+// Configurar resolvers
 
-const app = express()
-const port = process.env.port || 3000
-const schema = buildSchema(`type Query {
-    hello: String,
-}`);
-//Configurar resolvers
-
-const resolvers = {
-  hello: () => "Hola Mundo",
-};
-
-
-app.use('/api',gqlMiddleware({
+app.use(
+  "/api",
+  gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
     graphiql: true
-}))
-app.listen(port,()=>{
-    console.log(`Server is listening at port http://localhost:${port}/api`)
-})
+  })
+);
+app.listen(port, () => {
+  console.log(`Server is listening at port http://localhost:${port}/api`);
+});
