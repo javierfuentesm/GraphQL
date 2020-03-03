8a5d2c47153908798ab9adf9418aff4fa057e550
Javier <javier.fuentesm@hotmail.com>
2020-03-02 19:26:20 -0600

    Unions y se impelmenta una busqueda

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 "use strict";
-require('dotenv').config()
+require("dotenv").config();
 const { makeExecutableSchema } = require("graphql-tools");
 const express = require("express");
+const cors = require("cors");
 const gqlMiddleware = require("express-graphql");
 const { readFileSync } = require("fs");
 const { join } = require("path");
 const resolvers = require("./lib/resolvers");
 const app = express();
 const port = process.env.port || 3000;
+const isDev= process.env.NODE_ENV !== 'production'
 const typeDefs = readFileSync(
   join(__dirname, "lib", "schema.graphql"),
   "utf-8"
@@ -15,13 +17,13 @@ const typeDefs = readFileSync(
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 // Configurar resolvers
-
+app.use(cors());
 app.use(
   "/api",
   gqlMiddleware({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true
+    graphiql: isDev
   })
 );
 app.listen(port, () => {
